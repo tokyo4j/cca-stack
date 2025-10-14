25278ee7d9f9dce829db468bfbd5d48fd82cd1af
tokyo4j <hrak1529@gmail.com>
2025-10-14 15:36:58 +0900

    [wip] add SET_PAGES_UNMERGEABLE

--- gtest.c
+++ gtest.c
@@ -4,11 +4,11 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#define NR_PAGES (256 * 1024) // 1024MB
+#define NR_PAGES (1)
 
 int main(int argc, char *argv[]) {
 	puts("AppStart");
-	sleep(30);
+	//sleep(30);
 
 	puts("AllocStart");
 	char *ptr = mmap(0, 4096 * NR_PAGES, PROT_READ | PROT_WRITE,
@@ -16,16 +16,17 @@ int main(int argc, char *argv[]) {
 	for (uint32_t i = 0; i < NR_PAGES; i++)
 		*(uint32_t *)&ptr[4096 * i] = i;
 	puts("AllocEnd");
-	sleep(30);
+	//sleep(30);
 
 	if (argc == 2 && !strcmp(argv[1], "--no-rme")) {
 		puts("KsmStart");
-		sleep(30);
+		//sleep(30);
 	} else {
 		puts("MadviseStart");
 		madvise(ptr, 4096 * NR_PAGES, 26);
+		madvise(ptr, 4096 * NR_PAGES, 27);
 		puts("MadviseEnd");
-		sleep(30);
+		//sleep(30);
 	}
 
 	puts("LoopStart");
