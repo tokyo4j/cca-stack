d8500213cf9b677e303d2cd48b2bc59b2ac24018
tokyo4j <hrak1529@gmail.com>
2025-11-28 21:15:00 +0900

    Update figure for microbenchmark latency

--- fig-micro-latency.py
+++ fig-micro-latency.py
@@ -3,133 +3,133 @@ import matplotlib
 import sys
 import parse
 
-"""
-- Run no app
-- Set is_victim=0 and is_attacker=0
-- RMM@ff6ee28737aa232bb766b871370e578b02f40957
-"""
-
 fm = matplotlib.font_manager
 fm._get_fontconfig_fonts.cache_clear()
-
-d_victim = []
-d_attacker = []
-d_reclaim = []
-
-for line in open("data/2025-11-27_19-43-06/output-host-0.txt").readlines():
-    if r_victim := parse.search("[{ts:^f}] From victim: ipa={ipa:x}", line):
-        d_victim.append(
-            {
-                "ts": r_victim.named["ts"],
-                "ipa": f"0x{r_victim.named['ipa']:x}",
-            }
-        )
-    elif r_attacker := parse.search("[{ts:^f}] From attacker: ipa={ipa:x}", line):
-        d_attacker.append(
-            {
-                "ts": r_attacker.named["ts"],
-                "ipa": f"0x{r_attacker.named['ipa']:x}",
-            }
-        )
-    elif r_reclaim := parse.search(
-        "[{ts:^f}] [{:d}] Reclaimed pa={pa:x} ipa={ipa:x}", line
-    ):
-        d_reclaim.append(
-            {
-                "ts": r_reclaim.named["ts"],
-                "ipa": f"0x{r_reclaim.named['ipa']:x}",
-                "pa": f"0x{r_reclaim.named['pa']:x}",
-            }
-        )
-
-print(len(d_reclaim))
 
 plt.rcParams["font.family"] = "Noto Sans CJK JP"
 plt.figure(figsize=(10, 6))
 
-plt.plot(
-    [d["ts"] for d in d_victim],
-    [d["ipa"] for d in d_victim],
-    color="b",
-    label="Victim realm sets mergeable page",
-    marker=".",
-    linestyle="",
-    markersize=4,
-)
-
-plt.plot(
-    [d["ts"] for d in d_attacker],
-    [d["ipa"] for d in d_attacker],
-    color="r",
-    label="Attacker realm sets mergeable page",
-    marker=".",
-    linestyle="",
-    markersize=4,
-)
-
-plt.plot(
-    [d["ts"] for d in d_reclaim],
-    [d["ipa"] for d in d_reclaim],
-    color="g",
-    label="VMM reclaims page from RMM",
-    marker=".",
-    linestyle="",
-    markersize=4,
-)
-
-
-def draw_line(plt, p1, p2, color):
+def draw_line(plt, p1, p2):
     plt.plot(
         [p1[0], p2[0]],
         [p1[1], p2[1]],
-        color=color,
+        color="m",
         alpha=0.4,
         marker="",
-        linewidth=0.5,
+        zorder=5,
     )
 
+def plot(data_dir, out_filename):
+    plt.figure(figsize=(10, 6))
+    plt.xlim(100, 210)
+    d_victim = []
+    d_attacker = []
+    d_reclaim = []
 
-for line in open("data/2025-11-27_19-43-06/output-firmware.txt").readlines():
-    if r := parse.search(
-        "scan->ipa={scan_ipa:x}, dup->ipa={dup_ipa:x}, rand->pa={rand_pa:x}, rand->ipa={rand_ipa:x}",
-        line,
-    ):
-        scan_ipa = f"0x{r.named['scan_ipa']:x}"
-        dup_ipa = f"0x{r.named['dup_ipa']:x}"
-        rand_pa = f"0x{r.named['rand_pa']:x}"
-        rand_ipa = f"0x{r.named['rand_ipa']:x}"
+    for line in open(f"{data_dir}/output-host-0.txt").readlines():
+        if r_victim := parse.search("[{ts:^f}] From victim: ipa={ipa:x}", line):
+            d_victim.append(
+                {
+                    "ts": r_victim.named["ts"],
+                    "ipa": f"0x{r_victim.named['ipa']:x}",
+                }
+            )
+        elif r_attacker := parse.search("[{ts:^f}] From attacker: ipa={ipa:x}", line):
+            d_attacker.append(
+                {
+                    "ts": r_attacker.named["ts"],
+                    "ipa": f"0x{r_attacker.named['ipa']:x}",
+                }
+            )
+        elif r_reclaim := parse.search(
+            "[{ts:^f}] [{:d}] Reclaimed pa={pa:x} ipa={ipa:x}", line
+        ):
+            d_reclaim.append(
+                {
+                    "ts": r_reclaim.named["ts"],
+                    "ipa": f"0x{r_reclaim.named['ipa']:x}",
+                    "pa": f"0x{r_reclaim.named['pa']:x}",
+                }
+            )
 
-        scan = [d for d in d_attacker if d["ipa"] == scan_ipa] + [
-            d for d in d_victim if d["ipa"] == scan_ipa
-        ]
-        dup = [d for d in d_attacker if d["ipa"] == dup_ipa] + [
-            d for d in d_victim if d["ipa"] == dup_ipa
-        ]
-        rand = [d for d in d_attacker if d["ipa"] == rand_ipa] + [
-            d for d in d_victim if d["ipa"] == rand_ipa
-        ]
-        assert len(scan) == 1
-        assert len(dup) == 1
-        assert len(rand) == 1
-        scan = scan[0]
-        dup = dup[0]
-        rand = rand[0]
-        ret = [d for d in d_reclaim if d["pa"] == rand_pa][0]
+    plt.plot(
+        [d["ts"] for d in d_victim],
+        [d["ipa"] for d in d_victim],
+        color="b",
+        label="Victim realm sets mergeable page",
+        marker=".",
+        linestyle="",
+        markersize=6,
+        zorder=10,
+    )
 
-        p1 = scan["ts"], scan["ipa"]
-        p2 = dup["ts"], dup["ipa"]
-        p3 = rand["ts"], rand["ipa"]
-        p4 = ret["ts"], ret["ipa"]
-        draw_line(plt, p1, p4, "m")
-        draw_line(plt, p2, p4, "m")
-        # draw_line(plt, p3, p4, "g")
+    plt.plot(
+        [d["ts"] for d in d_attacker],
+        [d["ipa"] for d in d_attacker],
+        color="r",
+        label="Attacker realm sets mergeable page",
+        marker=".",
+        linestyle="",
+        markersize=6,
+        zorder=10,
+    )
 
-plt.legend()
-plt.ylabel("Guest physical address", fontsize=17)
-plt.xlabel("Time (s)", fontsize=17)
+    plt.plot(
+        [d["ts"] for d in d_reclaim],
+        [d["ipa"] for d in d_reclaim],
+        color="g",
+        label="VMM reclaims page from RMM",
+        marker=".",
+        linestyle="",
+        markersize=6,
+        zorder=10,
+    )
 
-plt.grid(True)
-plt.gca().xaxis.set_tick_params(labelsize=16)
-plt.gca().yaxis.set_tick_params(labelsize=8)
-plt.gca().yaxis.set_ticklabels([])
-plt.savefig("fig-micro-latency.pdf")
+    for line in open(f"{data_dir}/output-firmware.txt").readlines():
+        if r := parse.search(
+            "p1->ipa={p1_ipa:x}, p2->ipa={p2_ipa:x}, ret->pa={ret_pa:x}, ret->ipa={ret_ipa:x}",
+            line,
+        ):
+            p1_ipa = f"0x{r.named['p1_ipa']:x}"
+            p2_ipa = f"0x{r.named['p2_ipa']:x}"
+            ret_pa = f"0x{r.named['ret_pa']:x}"
+            ret_ipa = f"0x{r.named['ret_ipa']:x}"
+
+            p1_marked = [d for d in d_attacker if d["ipa"] == p1_ipa] + [
+                d for d in d_victim if d["ipa"] == p1_ipa
+            ]
+            p2_marked = [d for d in d_attacker if d["ipa"] == p2_ipa] + [
+                d for d in d_victim if d["ipa"] == p2_ipa
+            ]
+            ret_marked = [d for d in d_attacker if d["ipa"] == ret_ipa] + [
+                d for d in d_victim if d["ipa"] == ret_ipa
+            ]
+            assert len(p1_marked) == 1
+            assert len(p2_marked) == 1
+            assert len(ret_marked) == 1
+            p1_marked = p1_marked[0]
+            p2_marked = p2_marked[0]
+            ret_marked = ret_marked[0]
+            ret_reclaimed = [d for d in d_reclaim if d["pa"] == ret_pa][0]
+
+            p1_marked_p = p1_marked["ts"], p1_marked["ipa"]
+            p2_marked_p = p2_marked["ts"], p2_marked["ipa"]
+            ret_marked_p = ret_marked["ts"], ret_marked["ipa"]
+            ret_reclaimed_p = ret_reclaimed["ts"], ret_reclaimed["ipa"]
+            draw_line(plt, p1_marked_p, ret_reclaimed_p)
+            draw_line(plt, p2_marked_p, ret_reclaimed_p)
+
+    plt.legend(loc="upper left")
+    plt.ylabel("Guest physical address", fontsize=17)
+    plt.xlabel("Time (s)", fontsize=17)
+
+    plt.grid(True)
+    plt.gca().xaxis.set_tick_params(labelsize=16)
+    plt.gca().yaxis.set_tick_params(labelsize=8)
+    plt.gca().yaxis.set_ticklabels([])
+    plt.savefig(out_filename)
+
+plot("data/2025-11-28_20-43-05-no-rand", "fig-micro-latency-no-rand.pdf")
+plot("data/2025-11-28_20-27-48-no-spatial-rand", "fig-micro-latency-no-spatial-rand.pdf")
+plot("data/2025-11-28_20-37-42-no-temporal-rand", "fig-micro-latency-no-temporal-rand.pdf")
+plot("data/2025-11-28_20-47-33-rand", "fig-micro-latency-rand.pdf")
